4a058222c5bf07dcb9a5749e4193a6358dbefee9
nserekoc11 <85781633+nserekoc11@users.noreply.github.com>
2026-03-07 12:58:46 +0300

    css update before brake point

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, json, render_template, request, request
 
 app = Flask(__name__)
 
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+#JSON Formatter Route
+@app.route('/json', methods=['GET', 'POST'])
+def json_formatter():
+    formatted = ""
+    error = ""
+    if request.method == 'POST':
+        data = request.form['json_input'] #get user input
+        try:
+            parsed = json.loads(data) #parse the JSON data
+            formatted = json.dumps(parsed, indent=4) #format the JSON data
+        except Exception as e:
+            error = str(e)
+    return render_template('json_formatter.html', formatted=formatted, error=error) #handle JSON parsing errors
 if __name__ == '__main__':
     app.run(debug=True)
